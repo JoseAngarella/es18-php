dfb967c2b1217569b16cf7f7b96298c9ca9b21b8
Benncio09 <lorebencini13@outlook.com>
2024-10-10 08:41:07 +0200

    finit e funzionante

--- esercizio18.php
+++ esercizio18.php
@@ -6,6 +6,18 @@
     <title>Document</title>
 </head>
 <body>
+    <?php
+        $film=array("titanic", "spiderman", "transformers", "lo squalo", "avatar");
+        $menu="<select name='film preferito'>";
+        for ($i=0; $i<count($film); $i++ ){
+            $numeroFilm=$i+1;
+            $menu=$menu."<option value='f{$numeroFilm}'>{$film[$i]}</option>";
+
+        }
+        $menu=$menu."</select>";
+        echo $menu;
+
+    ?>
     
 </body>
 </html>
